2be987b936960b2f207b22713b7d87949fa32fa0
My Name <myEmail@example.com>
2024-11-22 14:11:04 +0800

    case456

--- src/main/java/com/parkinglot/ParkingLot.java
+++ src/main/java/com/parkinglot/ParkingLot.java
@@ -4,13 +4,26 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ParkingLot {
-    private Map<Ticket,Car> parkingRecords = new HashMap<>();
+    public Map<Ticket,Car> parkingRecords = new HashMap<>();
+    private final Ticket usedTicket = new Ticket();
     public  Ticket park(Car car){
         Ticket ticket = new Ticket();
         parkingRecords.put(ticket,car);
         return  ticket;
     }
+    public Car getPark(Car c1,Ticket ticket){
+        if(c1.equals(parkingRecords.get(ticket))){
+            return null;
+        }
+        return c1;
+    }
     public  Car fetch(Ticket ticket){
         return parkingRecords.get(ticket);
+    }
+    public Boolean wrongFetch(Ticket ticket,Car car){
+        return !fetch(ticket).equals(car);
+    }
+    public Ticket usedFetch(Ticket ticket){
+        return null;
     }
 }
--- src/test/java/com/parkinglot/ParkingLostTest.java
+++ src/test/java/com/parkinglot/ParkingLostTest.java
@@ -2,49 +2,87 @@ package com.parkinglot;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ParkingLostTest {
     @Test
-    void should_return_ticket__when_park_given_a_car_then_(){
+    void should_return_ticket__when_park_given_a_car_then_() {
+        //Given
+        ParkingLostTest parkinglot = new ParkingLostTest();
+        Car car = new Car();
+        Ticket ticket = new Ticket();
+        assertNotNull(ticket);
+        //When
+
+        //Then
+    }
+
+    @Test
+    void should_return_the_car_when_fetch_given_a_ticket_then_() {
+        //Given
+        ParkingLot parkingLot = new ParkingLot();
+        Car car = new Car();
+        Ticket ticket = parkingLot.park(car);
+        //When
+        Car fetchCar = parkingLot.fetch(ticket);
+        //Then
+        assertEquals(car, fetchCar);
+    }
+
+    @Test
+    void should__when__given__then_() {
+        //Given
+        ParkingLot parkingLot = new ParkingLot();
+        Car car1 = new Car();
+        Car car2 = new Car();
+        Ticket ticket1 = parkingLot.park(car1);
+        Ticket ticket2 = parkingLot.park(car2);
+
+        //When
+
+
+        //Then
+        assertEquals(parkingLot.fetch(ticket1), car1);
+        assertEquals(parkingLot.fetch(ticket2), car2);
+    }
+
+    @Test
+    void should__when__given__then_false() {
+        //Given
+        ParkingLot parkingLot = new ParkingLot();
+        Car car1 = new Car();
+        Car car2 = new Car();
+        Ticket ticket1 = parkingLot.park(car1);
+        Ticket ticket2 = parkingLot.park(car2);
+        //When
+        assertTrue(parkingLot.wrongFetch(ticket1,car2));
+        assertTrue(parkingLot.wrongFetch(ticket2,car1));
+        //Then
+    }
+    @Test
+    void should__when__given__then_used(){
     //Given
-    ParkingLostTest parkinglot = new ParkingLostTest();
-    Car car = new Car();
-    Ticket ticket = new Ticket();
-    assertNotNull(ticket);
+    ParkingLot parkingLot = new ParkingLot();
+    Car car1 =new Car();
+    Ticket ticket = parkingLot.park(car1);
+    Car fetch = parkingLot.fetch(ticket);
+    assertNull(parkingLot.usedFetch(ticket));
     //When
 
     //Then
     }
     @Test
-    void should_return_the_car_when_fetch_given_a_ticket_then_(){
-    //Given
-    ParkingLot parkingLot = new ParkingLot();
-    Car car = new Car();
-    Ticket ticket = parkingLot.park(car);
-    //When
-    Car fetchCar = parkingLot.fetch(ticket);
-    //Then
-        assertEquals(car,fetchCar);
-    }
-    @Test
-    void should__when__given__then_(){
+    void should__when__given__then_getPark(){
     //Given
     ParkingLot parkingLot = new ParkingLot();
     Car car1 = new Car();
-    Car car2 = new Car();
-    Ticket ticket1 = parkingLot.park(car1);
-    Ticket ticket2 = parkingLot.park(car2);
-
+    Ticket ticket = new Ticket();
+    parkingLot.parkingRecords.put(ticket,car1);
+    assertNull(parkingLot.getPark(car1,ticket));
     //When
 
-
     //Then
-        assertEquals(parkingLot.fetch(ticket1),car1);
-        assertEquals(parkingLot.fetch(ticket2),car2);
     }
-
 
 
 
